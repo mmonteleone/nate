11e9017633c139f5cc7d8b6db4236671b5eb2db7
Michael Monteleone <michael@michaelmonteleone.net>
2009-09-17 01:23:45 -0500

    more core model unit testing

---
+++ src/Nate.Tests/Unit/Core/TransitionEventArgsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nate.Core;
+using Xunit;
+using Moq;
+
+namespace Nate.Tests.Unit.Core
+{
+    public class TransitionEventArgsTests
+    {
+        [Fact]
+        public void TransitionEventArgs_NullModel_ThrowsNullEx()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new TransitionEventArgs<StubStateModel>(
+                    null,
+                    new State<StubStateModel>("from"),
+                    new State<StubStateModel>("to"),
+                    new Trigger("trigger")));
+        }
+
+        [Fact]
+        public void TransitionEventArgs_NullFrom_ThrowsNullEx()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new TransitionEventArgs<StubStateModel>(
+                    new StubStateModel(),
+                    null,
+                    new State<StubStateModel>("to"),
+                    new Trigger("trigger")));
+        }
+
+        [Fact]
+        public void TransitionEventArgs_NullTo_ThrowsNullEx()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new TransitionEventArgs<StubStateModel>(
+                    new StubStateModel(),
+                    new State<StubStateModel>("from"),
+                    null,
+                    new Trigger("trigger")));
+        }
+
+        [Fact]
+        public void TransitionEventArgs_NullTrigger_ThrowsNullEx()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new TransitionEventArgs<StubStateModel>(
+                    new StubStateModel(),
+                    new State<StubStateModel>("from"),
+                    new State<StubStateModel>("to"),
+                    null));
+        }
+
+        [Fact]
+        public void TransitionEventArgs_ValidParms_VerifyAssigns()
+        {
+            var model = new StubStateModel();
+            var from = new State<StubStateModel>("from");
+            var to = new State<StubStateModel>("to");
+            var trigger = new Trigger("trigger");
+            var args = new TransitionEventArgs<StubStateModel>(model, from, to, trigger);
+
+            Assert.Same(from, args.From);
+            Assert.Same(to, args.To);
+            Assert.Same(model, args.Model);
+            Assert.Same(trigger, args.Trigger);
+        }
+    }
+}
---
+++ src/Nate.Tests/Unit/Core/TriggerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Moq;
+using Nate.Core;
+
+namespace Nate.Tests.Unit.Core
+{
+    public class TriggerTests
+    {
+        [Fact]
+        public void Trigger_NullName_ThrowsNullEx()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Trigger(null));
+        }
+
+        [Fact]
+        public void Trigger_Name_VerifyAssign()
+        {
+            var name = "trigger";
+            var trigger = new Trigger(name);
+            Assert.Equal(name, trigger.Name);
+        }
+
+        [Fact]
+        public void Trigger_ToString_VerifySameAsName()
+        {
+            var name = "trigger";
+            var trigger = new Trigger(name);
+            Assert.Equal(name, trigger.ToString());
+        }
+
+        [Fact]
+        public void Trigger_GetHashCode_VerifySameAsNameHash()
+        {
+            var name = "trigger";
+            var trigger = new Trigger(name);
+            Assert.Equal(name.GetHashCode(), trigger.GetHashCode());
+        }
+
+        [Fact]
+        public void Trigger_Equals_TwoInstances_SameName_ReturnTrue()
+        {
+            var trigger1 = new Trigger("trigger");
+            var trigger2 = new Trigger("trigger");
+            Assert.True(trigger1.Equals(trigger2));
+        }
+    }
+}
