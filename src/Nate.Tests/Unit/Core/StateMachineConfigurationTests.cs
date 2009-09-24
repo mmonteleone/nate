da02341a62d15c2bc585ac4f52f3397616f6ef99
Michael Monteleone <michael@michaelmonteleone.net>
2009-09-23 21:58:06 -0500

    core model unit tests completed

---
+++ src/Nate.Tests/Unit/Core/StateMachineConfigurationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nate.Core;
+using Xunit;
+
+namespace Nate.Tests.Unit.Core
+{
+    public class StateMachineConfigurationTests
+    {
+        [Fact]
+        public void StateMachineConfiguration_RaiseExceptionOnTriggerMatchingNoTransitions_DefaultsTrue()
+        {
+            Assert.True((new StateMachineConfiguration()).RaiseExceptionOnTriggerMatchingNoTransition);
+        }
+
+        [Fact]
+        public void StateMachineConfiguration_RaiseExceptionOnTriggerMatchingNoPassingTransitions_DefaultsFalse()
+        {
+            Assert.False((new StateMachineConfiguration()).RaiseExceptionOnTriggerMatchingNoPassingTransition);
+        }
+
+        [Fact]
+        public void StateMachineConfiguration_RaiseExceptionOnTransitioningToSameState_DefaultsFalse()
+        {
+            Assert.False((new StateMachineConfiguration()).RaiseExceptionBeforeTransitionToSameState);
+        }
+    }
+}
