43de61ef4a1a63b2bef4154aca8858c68136ff20
Dariusz Danielewski <dydek@hotmail.com>
2022-01-02 18:12:03 -0500

    Doc updates

--- src/build-nuget-packages.ps1
+++ src/build-nuget-packages.ps1
@@ -5,7 +5,7 @@ pushd .\Nate
 Write-Output "Building release $versionSuffix nuget packages..."
 dotnet pack --configuration Release --include-symbols --version-suffix $versionSuffix
 Write-Output "Moving $versionSuffix nuget packages to releases folder..."
-If(!(test-path ..\releases))
+If(!(test-path ..\..\releases))
 {
       New-Item -ItemType Directory -Force -Path ..\..\releases
 }
