6081eaeaa8f966990052d3c3ab189fc56fb5a45d
Dariusz Danielewski <dydek@hotmail.com>
2022-01-02 16:35:18 -0500

    Code cleanup

--- src/Nate/Core/Extensions/StateExtensions.cs
+++ src/Nate/Core/Extensions/StateExtensions.cs
@@ -46,9 +46,9 @@ namespace Nate.Core.Extensions
             Trigger trigger,
             State<TStateModel> to) where TStateModel : IStateModel
         {
-            if (state == null) throw new ArgumentNullException("state");
-            if (trigger == null) throw new ArgumentNullException("trigger");
-            if (to == null) throw new ArgumentNullException("to");
+            if (state == null) throw new ArgumentNullException(nameof(state));
+            if (trigger == null) throw new ArgumentNullException(nameof(trigger));
+            if (to == null) throw new ArgumentNullException(nameof(to));
 
             state.AddTransition(
                 new Transition<TStateModel>(trigger, state, to));
@@ -67,10 +67,10 @@ namespace Nate.Core.Extensions
             State<TStateModel> to,
             Func<TStateModel, bool> guard) where TStateModel : IStateModel
         {
-            if (state == null) throw new ArgumentNullException("state");
-            if (trigger == null) throw new ArgumentNullException("trigger");
-            if (to == null) throw new ArgumentNullException("to");
-            if (guard == null) throw new ArgumentNullException("guard");
+            if (state == null) throw new ArgumentNullException(nameof(state));
+            if (trigger == null) throw new ArgumentNullException(nameof(trigger));
+            if (to == null) throw new ArgumentNullException(nameof(to));
+            if (guard == null) throw new ArgumentNullException(nameof(guard));
 
             state.AddTransition(
                 new Transition<TStateModel>(trigger, state, to, guard));
